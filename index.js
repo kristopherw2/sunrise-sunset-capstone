da77d4f13312dfcb91312504f7715305920a7133
Kristopher Williams <kristopher.williams2@gmail.com>
2019-08-04 18:58:12 -0500

    apis found

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 "use strict"
+// mapquest api = https://developer.mapquest.com/documentation/geocoding-api/
+//sunrise api = https://sunrise-sunset.org/api
+
 
 const apikey = ""
+
+function getUserLatLong (location) {
+    
+}
+
 
 function main () {
     $('form').on('click', '#user-click', function (){
