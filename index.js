223e2901e0a77e822f3a6280efcb7d153ceb2712
Kristopher Williams <kristopher.williams2@gmail.com>
2019-08-04 20:53:26 -0500

    added sunrise and mapquest fetch calls

--- index.js
+++ index.js
@@ -5,52 +5,72 @@
 
 
 const apikey = "792a50c1d3fd55e54cf1432083cf34d73c8c6d2a"
-const sunriseURL = "https://sunrise-sunset.org/api"
+const sunriseURL = "https://sunrise-sunset.org/api/json"
 const mapquestURL = "https://developer.mapquest.com/documentation/geocoding-api/"
+const mapquestLatLong = {}; 
+const userDateSelected;
 
-
-/*
-function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-    console.log(queryItems.join('&'));
-  return queryItems.join('&');
-}
-function createVideoHTML(title, description, thumbnailURL) {
-    return `
-        <div class="video">
-            <img class="video-thumbnail" src="${thumbnailURL}" />
-            <div class="video-title">${title}</div>
-            <div class="video-description">${description}</div>
-        </div>`}
-function displayResults(responseData) {
-    const items = responseData.items
-    const itemHtml = items.map(function(item){
-        const title = (item.snippet.title)
-        const description = (item.snippet.description)
-        const thumbnail = (item.snippet.thumbnails.medium.url)
-        return createVideoHTML(title, description, thumbnail)
-    }).join("")
-    $("#search-results").html(itemHtml)
-}*/
 
 //sets up event listeners
 function main () {
     $('form').on('click', '#user-click', function (){
         let userLocation = $('#user-input-location').val();
-        let userDate = $('#user-input-date').val();
+        userDateSelected = $('#user-input-date').val();
         console.log(userLocation, userDate);
     })
     getUserMapquestInfo(userLocation);
 }
 
-//builds object data for mapquest api
+//builds object data for mapquest api and fetches lattitude and longitude from JSON and stores the values in mapquestLatLong
 function getUserMapquestInfo (location) {
   const mapquestInfo = {
     "key": apikey,
     "location": location
   }
-  formatMapquestQuery(mapquestInfo);
+  const mapquestQueryInfo = formatMapquestQuery(mapquestInfo);
+  const mapquestSearchURL = mapquestURL + '?' + mapquestQueryInfo; 
+
+  fetch(mapquestSearchURL)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  })
+  .then(responseJson => {
+    console.log(JSON.stringify(responseJson));
+    return mapquestLatLong = responseJson.results.locations.displayLatLng
+  })
+  .catch(err => {
+    $('#js-error-message').text(`Something went wrong: ${err.message}`);
+  });
+
+}
+
+//stores lat and lng in string for sunrise call
+let mapquestLat = `lat=${mapquestLatLong.lat}`
+let mapquestLng = `lng=${mapquestLatLong.lng}`
+
+
+
+//makes the call to the sunrise api
+function sunriseQuery(mapquestLatLong, userDateSelected) {
+  const sunriseURLQuery =sunriseURL + '?' + mapquestlat + mapquestLng + userDateSelected;
+  
+  fetch(sunriseURLQuery)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  })
+  .then(responseJson => {
+    console.log(JSON.stringify(responseJson));
+    sunriseResultsData(responseJson);
+  })
+  .catch(err => {
+    $('#js-error-message').text(`Something went wrong: ${err.message}`);
+  });
 }
 
 //formats data for params for api query
@@ -60,12 +80,10 @@ function formatMapquestQuery (mapquestObjectData) {
   return mapquestQueryItems.join('&');
 }
 
-
-
-
 //the results
-function sunriseResultsData() {
-$('.search-results').empty()
+function sunriseResultsData(responseJSON) {
+  responseJSON.map()
+  $('.search-results').empty()
 return $('.search-results').html(
   `<div id="sunriseTime"><span>Sunrise time: ${responseJSON.results.sunrise}<b>
   Sunset Time: ${responseJSON.results.sunset}<b>
