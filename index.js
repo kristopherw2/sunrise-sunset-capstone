e702a897af4fa0f10fc2889277fc3bcb9a6dbbc4
Kristopher Williams <kristopher.williams2@gmail.com>
2019-08-06 19:13:10 -0500

    finished mvp

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 "use strict";
-// mapquest api = https://developer.mapquest.com/documentation/geocoding-api/
-//sunrise api = https://sunrise-sunset.org/api
-
-
 
 const apikey = "zkn6RGyTDlLGsN8i8RfEmURf2GozTAkL"
-const sunriseURL = "https://sunrise-sunset.org/api/json"
+const sunriseURL = "https://api.sunrise-sunset.org/json"
 const mapquestURL = "http://www.mapquestapi.com/geocoding/v1/address"
-const mapquestLatLong = {}; 
-let userDateSelected = ""
-
-
-
+const mapquestLatLong = {};
+let userDateSelected = 0;
 
 //builds object data for mapquest api and fetches lattitude and longitude from JSON and stores the values in mapquestLatLong
 function getUserMapquestInfo (location) {
@@ -30,26 +23,28 @@ function getUserMapquestInfo (location) {
     throw new Error(response.statusText);
   })
   .then(responseJson => {
-    console.log(JSON.stringify(responseJson));
-    return mapquestLatLong = responseJson.results.locations.displayLatLng
+    const mapquestLat = `${responseJson.results[0].locations[0].latLng.lat}`
+    const mapquestLng = `${responseJson.results[0].locations[0].latLng.lng}`
+    sunriseQuery(mapquestLat, mapquestLng, userDateSelected);
   })
   .catch(err => {
-    $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    console.log(`${err.message}`);
   });
 
 }
 
-//stores lat and lng in string for sunrise call
-let mapquestLat = `lat=${mapquestLatLong.lat}`
-let mapquestLng = `lng=${mapquestLatLong.lng}`
-
-
-
 //makes the call to the sunrise api
 function sunriseQuery(mapquestLat, mapquestLng, userDateSelected) {
-  const sunriseURLQuery =sunriseURL + '?' + mapquestLat + mapquestLng + userDateSelected;
-  
-  fetch(sunriseURLQuery)
+  const sunriseParams = {
+       "lat": mapquestLat,
+      'lng': mapquestLng,
+      "date": userDateSelected
+  }
+
+  const sunriseQueryInfo = formatMapquestQuery(sunriseParams);
+  const completedSunriseURL = sunriseURL + '?' + sunriseQueryInfo;
+    
+  fetch(completedSunriseURL)
   .then(response => {
     if (response.ok) {
       return response.json();
@@ -57,26 +52,21 @@ function sunriseQuery(mapquestLat, mapquestLng, userDateSelected) {
     throw new Error(response.statusText);
   })
   .then(responseJson => {
-    console.log(JSON.stringify(responseJson));
     sunriseResultsData(responseJson);
   })
   .catch(err => {
-    $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    console.log(`${err.message}`);
   });
 }
 
 //formats data for params for api query
 function formatMapquestQuery (mapquestObjectData) {
   const mapquestQueryItems = Object.keys(mapquestObjectData).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(mapquestObjectData[key])}`)
-  console.log(mapquestQueryItems);
   return mapquestQueryItems.join('&');
 }
 
 //the results
 function sunriseResultsData(responseJSON) {
-  //$('.search-results').empty()
-  //responseJSON.map()
-  $('.search-results').removeAttr('hidden')
  return $('.search-results').html(
   `<div id="sunriseTime"><span>Sunrise time: ${responseJSON.results.sunrise}<b>
   Sunset Time: ${responseJSON.results.sunset}<b>
