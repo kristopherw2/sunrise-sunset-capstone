aef9ae61b59db12b989e90203e68fc38b4612a7b
Kristopher Williams <kristopher.williams2@gmail.com>
2019-08-15 23:07:36 -0500

    adjusted results divs

--- index.js
+++ index.js
@@ -55,66 +55,68 @@ function getUserMapquestInfo(location) {
     })
     .then(responseJson => {
 
-      //checks the Json for what should be valid entries
-      if (responseJson.results[0].locations[0].adminArea3 === "" || responseJson.results[0].locations[0].adminArea4 === "") {
-        $('.error-messages').text('Please enter a valid location')
-       return $('#results-header').css('display', 'none');
-      } 
+        //checks the Json for what should be valid entries
+        if (!responseJson.results[0].locations || responseJson.results[0].locations.length == 0) {
+            $('.error-messages').text('Please enter a valid location')
+            return $('#results-header').css('display', 'none');
+          } else if (responseJson.results[0].locations[0].adminArea3 === "" || responseJson.results[0].locations[0].adminArea4 === "") {
+            $('.error-messages').text('Please enter a valid location')
+            return $('#results-header').css('display', 'none');
+          } else {
+            mapquestLat = `${responseJson.results[0].locations[0].latLng.lat}`
+            mapquestLng = `${responseJson.results[0].locations[0].latLng.lng}`
+            sunriseQuery(mapquestLat, mapquestLng, userDateSelected);
+            getStaticMap(responseJson.results[0].providedLocation.location);
+          }
 
-        mapquestLat = `${responseJson.results[0].locations[0].latLng.lat}`
-        mapquestLng = `${responseJson.results[0].locations[0].latLng.lng}`
-        sunriseQuery(mapquestLat, mapquestLng, userDateSelected);
-        getStaticMap(responseJson.results[0].providedLocation.location);
-        
-    
-    })
-    .catch(err => {
-      $('.error-messages').text(`${err.message}`)
-    });
+        })
+      .catch(err => {
+        $('.error-messages').text(`${err.message}`)
+      });
 
-}
+    }
 
-//makes the call to the sunrise api
-function sunriseQuery(mapquestLat, mapquestLng, userDateSelected) {
-  const sunriseParams = {
-    "lat": mapquestLat,
-    'lng': mapquestLng,
-    "date": userDateSelected
+  //makes the call to the sunrise api
+  function sunriseQuery(mapquestLat, mapquestLng, userDateSelected) {
+    const sunriseParams = {
+      "lat": mapquestLat,
+      'lng': mapquestLng,
+      "date": userDateSelected
+    }
+
+    const sunriseQueryInfo = formatUserParamsQuery(sunriseParams);
+    const completedSunriseURL = sunriseURL + '?' + sunriseQueryInfo;
+
+    fetch(completedSunriseURL)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(response.statusText);
+      })
+      .then(responseJson => {
+        sunriseResultsData(responseJson);
+      })
+      .catch(err => {
+        $('.error-messages').txt(`${err.message}`)
+      });
   }
 
-  const sunriseQueryInfo = formatUserParamsQuery(sunriseParams);
-  const completedSunriseURL = sunriseURL + '?' + sunriseQueryInfo;
-
-  fetch(completedSunriseURL)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => {
-      sunriseResultsData(responseJson);
-    })
-    .catch(err => {
-      $('.error-messages').txt(`${err.message}`)
-    });
-}
-
-//formats data for params for api query
-function formatUserParamsQuery(mapquestObjectData) {
-  const mapquestQueryItems = Object.keys(mapquestObjectData).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(mapquestObjectData[key])}`)
-  return mapquestQueryItems.join('&');
-}
+  //formats data for params for api query
+  function formatUserParamsQuery(mapquestObjectData) {
+    const mapquestQueryItems = Object.keys(mapquestObjectData).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(mapquestObjectData[key])}`)
+    return mapquestQueryItems.join('&');
+  }
 
 
-//the results
-function sunriseResultsData(responseJSON) {
+  //the results
+  function sunriseResultsData(responseJSON) {
 
-  //makes date YYYY/MM/DD for iOS compatibility
-  const newDate = Object.keys(responseJSON.results).map(key => `${new Date(`${userDateSelected.replace(/-/g, '/')} ${responseJSON.results[key]} UTC`)}`).map(string => string.slice(0, 24)).map(string => string.concat(` Local Time`))
-  
-  $('.search-results').append(
-    `<div id="sunriseTime"><span>Sunrise: ${newDate[0]}<br>
+    //makes date YYYY/MM/DD for iOS compatibility
+    const newDate = Object.keys(responseJSON.results).map(key => `${new Date(`${userDateSelected.replace(/-/g, '/')} ${responseJSON.results[key]} UTC`)}`).map(string => string.slice(0, 24)).map(string => string.concat(` Local Time`))
+
+    $('.search-results').append(
+      `<div id="sunriseTime"><span>Sunrise: ${newDate[0]}<br>
   Sunset Time: ${newDate[1]}<br>
   Solar Noon: ${newDate[2]}</span></div>
   <div id="civilTime"><span>Civil Twilight Begin: ${newDate[4]}<br>
@@ -123,39 +125,39 @@ function sunriseResultsData(responseJSON) {
   Nautical Twilight End: ${newDate[7]}</span></div>
   <div id="astronomicalTime"><span>Astronomical Twilight Begin: ${newDate[8]}<br>
   Astronomical Twilight End: ${newDate[9]}</span></div>`).hide().fadeIn(1000)
-}
+  }
 
 
-//to draw map
-function drawMap(responseJson) {
-  $('.map').empty()
-  return $('.map').fadeIn(1000).append(`<img class="mapImg" src="${responseJson}" alt="picture of location">`).hide().fadeIn(1000);
-}
+  //to draw map
+  function drawMap(responseJson) {
+    $('.map').empty()
+    return $('.map').fadeIn(1000).append(`<img class="mapImg" src="${responseJson}" alt="picture of location">`).hide().fadeIn(1000);
+  }
 
-//sets up event listeners
-function main() {
-  $('form').on('submit', function (event) {
-    event.preventDefault();
-    let userLocation = $('#user-input-location').val();
-    userDateSelected = $('#user-input-date').val();
-    getUserMapquestInfo(userLocation);
-    $('h1, fieldset, .tribute-text, .intro-text').fadeOut(250)
-    $('#reset, #results-header').fadeIn(1000).css('display', 'block');
-    return userDateSelected;
-  })
-}
+  //sets up event listeners
+  function main() {
+    $('form').on('submit', function (event) {
+      event.preventDefault();
+      let userLocation = $('#user-input-location').val();
+      userDateSelected = $('#user-input-date').val();
+      getUserMapquestInfo(userLocation);
+      $('h1, fieldset, .tribute-text, .intro-text').fadeOut(250)
+      $('#reset, #results-header').fadeIn(1000).css('display', 'block');
+      return userDateSelected;
+    })
+  }
 
 
-//takes user back to start
-function reset() {
-  $('.reset').on('click', '#reset', function () {
-    $('#sunriseTime, #civilTime, #nauticalTime, #astronomicalTime, .mapImg, .results-header').remove();
-    $('h1, fieldset').fadeIn(1000);
-    $('.error-messages').empty()
-    $('#reset', ).css('display', 'none');
-    $('#results-header').css('display', 'none');
-    $('.tribute-text, .intro-text').fadeIn(1000)
-  })
-}
-$(main);
-$(reset);
+  //takes user back to start
+  function reset() {
+    $('.reset').on('click', '#reset', function () {
+      $('#sunriseTime, #civilTime, #nauticalTime, #astronomicalTime, .mapImg, .results-header').remove();
+      $('h1, fieldset').fadeIn(1000);
+      $('.error-messages').empty()
+      $('#reset', ).css('display', 'none');
+      $('#results-header').css('display', 'none');
+      $('.tribute-text, .intro-text').fadeIn(1000)
+    })
+  }
+  $(main);
+  $(reset);
